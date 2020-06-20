643519a53cb3e14b4a5f1ee2557e8da8c487d55c
Randy <randyisbusy@gmail.com>
2020-06-20 00:36:33 -0700

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 class Formatter {
-  //add static methods here
-  //add static methods here
+  //weird sometimes errors pop up and you can't be sure why
+
   static capitalize(string) {
-    return string[0].toUpperCase() + string.slice(1)
+
+    return string[0].toUpperCase() + string.slice(1) 
+
   }
 
-  static sanitize(string) {
+  static sanitize(string) { 
+
     return string.replace(/[^A-Za-z0-9\-\' ]+/g, '')
   }
   static titleize(string) {
-    const array = ["of", "the", "a", "an", "and", "but", "for", "at", "by", "from"]
+    const array = ["a", "the", "of", "an", "and", "but", "for", "at", "by", "from"]
 
     return string.split(" ").map(function (word, index) {
       if (index !== 0 && array.includes(word)) {
