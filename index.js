5e14f98938e34d114a561019850a2bd943fd686f
Randy <randyisbusy@gmail.com>
2020-06-20 01:16:52 -0700

    Done.

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ class Formatter {
   }
 
   static sanitize(string) { 
-
+    //these two files are not identical and this is irritating I cannot submit
     return string.replace(/[^A-Za-z0-9\-\' ]+/g, '') 
 
   }
   static titleize(string) {
-    const array = ["a", "the", "of", "an", "and", "but", "for", "at", "by", "from"]
+    const array = ["a", "the", "of", "an", "and", "but", "for", "by", "at", "from"]
 
     return string.split(" ").map(function (thing, index) {
       if (index !== 0 && array.includes(thing)) {
