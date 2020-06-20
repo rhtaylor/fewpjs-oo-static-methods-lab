fae24eae116a552ce1f41787bae9748964198b39
Randy <randyisbusy@gmail.com>
2020-06-20 00:40:42 -0700

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 class Formatter {
   //weird sometimes errors pop up and you can't be sure why
-
+  //I passed this lab and never submitted. Now having minor problems with it. 
   static capitalize(string) {
 
     return string[0].toUpperCase() + string.slice(1) 
@@ -9,16 +9,17 @@ class Formatter {
 
   static sanitize(string) { 
 
-    return string.replace(/[^A-Za-z0-9\-\' ]+/g, '')
+    return string.replace(/[^A-Za-z0-9\-\' ]+/g, '') 
+
   }
   static titleize(string) {
     const array = ["a", "the", "of", "an", "and", "but", "for", "at", "by", "from"]
 
-    return string.split(" ").map(function (word, index) {
-      if (index !== 0 && array.includes(word)) {
-        return word
+    return string.split(" ").map(function (thing, index) {
+      if (index !== 0 && array.includes(thing)) {
+        return thing
       } else {
-        return this.capitalize(word)
+        return this.capitalize(thing)
       }
     }.bind(this)).join(" ")
   }
